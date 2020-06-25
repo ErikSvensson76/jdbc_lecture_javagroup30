4e491d271cb59f73fec033cec3a4e9f3e0cef7bd
Erik Svensson <eosvensson@gmail.com>
2020-06-25 10:51:35 +0200

    Successfully added create method in AppUserDaoJDBC

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,10 @@
 package org.example;
 
-import org.example.data.MyDataSource;
+import org.example.data.AppUserDaoJDBC;
+import org.example.model.AppUser;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 /**
  * Hello world!
@@ -12,7 +12,18 @@ import java.sql.SQLException;
  */
 public class App 
 {
-    public static void main( String[] args ) throws SQLException {
+    public static void main( String[] args ) {
+        AppUserDaoJDBC daoJDBC = new AppUserDaoJDBC();
+
+        AppUser appUser = new AppUser(
+                "eriks",
+                "java_master",
+                "Erik Svensson",
+                LocalDate.parse("1976-09-11")
+        );
+
+        appUser = daoJDBC.create(appUser);
+        System.out.println(appUser);
 
     }
 }
--- src/main/java/org/example/data/AppUserDaoJDBC.java
+++ src/main/java/org/example/data/AppUserDaoJDBC.java
@@ -1,5 +1,63 @@
 package org.example.data;
 
+import org.example.model.AppUser;
+
+import java.sql.*;
+
 public class AppUserDaoJDBC {
+
+   public AppUser create(AppUser user){
+       if(user.getUserId() != 0){
+           throw new IllegalArgumentException("Use update instead");
+       }
+
+       Connection connection = null;
+       PreparedStatement statement = null;
+       ResultSet keySet = null;
+
+       try{
+
+           connection = MyDataSource.getConnection();
+           statement = connection.prepareStatement("INSERT INTO app_user (username, password, full_name, birth_date, active) VALUES (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+           statement.setString(1, user.getUsername());
+           statement.setString(2, user.getPassword());
+           statement.setString(3, user.getFullName());
+           statement.setObject(4, user.getBirthDate());
+           statement.setBoolean(5,user.isActive());
+
+           statement.execute();
+
+           keySet = statement.getGeneratedKeys();
+
+           while(keySet.next()){
+               user = new AppUser(
+                       keySet.getInt(1),
+                       user.getUsername(),
+                       user.getPassword(),
+                       user.getFullName(),
+                       user.getBirthDate(),
+                       user.isActive()
+               );
+           }
+
+       } catch (SQLException exception) {
+           exception.printStackTrace();
+       }finally {
+           try{
+               if(keySet != null){
+                   keySet.close();
+               }
+               if(statement != null){
+                   statement.close();
+               }
+               if(connection != null){
+                   connection.close();
+               }
+           }catch (SQLException ex){
+               ex.printStackTrace();
+           }
+       }
+       return user;
+   }
 
 }
